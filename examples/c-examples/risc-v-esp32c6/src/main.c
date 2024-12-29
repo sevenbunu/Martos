81ebc5c47ed973864094c599ebe4fb5756c3b0d4
Kostya <koreshin67@gmail.com>
2024-12-29 22:04:32 +0300

    ref: ref 69

--- examples/c-examples/risc-v-esp32c6/src/main.c
+++ examples/c-examples/risc-v-esp32c6/src/main.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <stdbool.h>
 
 extern void add_task(void (*setup_fn)(), void (*loop_fn)(), bool (*stop_condition_fn)());
@@ -18,7 +17,6 @@ bool stop_condition_fn() {
     if (counter == 50) {
         return true;
     }
-
     return false;
 }
 
@@ -26,16 +24,10 @@ int main( void ) {
     init_system();
     add_task(setup_fn, loop_fn, stop_condition_fn);
     start_task_manager();
-
-    // (Should never be reached)
     return 0;
 }
 
-// Application entry point / startup logic.
 void __attribute__( ( noreturn ) ) call_start_cpu0() {
-
-    // Done, branch to main
     main();
-    // (Should never be reached)
     while( 1 ) {}
 }
